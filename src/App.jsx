9b4ecf52fe305f99c072f1d8ffb31767339ee860
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-10 15:34:36 +0700

    42 Delete Todo

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,14 @@ const App = () => {
       id: randomIntFromInterval(1, 1000000),
       name: name
     }
-
     setTodoList([...todoList, newTodo])
   }
+  const deleteTodo = (id) => {
+    const newTodo = todoList.filter(item => item.id !== id);
+    setTodoList(newTodo);
+  }
+
+
 
   const randomIntFromInterval = (min, max) => { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -32,6 +37,7 @@ const App = () => {
 
       {todoList.length > 0 ?
         <TodoData
+          deleteTodo={deleteTodo}
           todoList={todoList}
         />
         :
--- src/component/todo/TodoData.jsx
+++ src/component/todo/TodoData.jsx
@@ -1,12 +1,18 @@
 const TodoData = (props) => {
-    const { todoList } = props;
+
+    const { todoList, deleteTodo } = props
+    const handleClickDelete = (id) => {
+        deleteTodo(id)
+    }
+    // const { todoList } = props;
     return (
+
         <div className='todo-data'>
             {todoList.map((item, index) => {
                 return (
                     <div className={`todo-item`} key={item.id}>
                         <div> {item.name}</div>
-                        <button>Delete</button>
+                        <button onClick={() => handleClickDelete(item.id)}>Delete</button>
                     </div>
                 )
             })}
