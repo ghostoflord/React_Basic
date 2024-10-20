126781521dff90e2429c257ec49634baf628e1a2
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-20 21:23:10 +0700

    75 Lifting State Up

--- src/component/user/user.table.jsx
+++ src/component/user/user.table.jsx
@@ -1,22 +1,9 @@
 import { Table } from 'antd';
 
-import { useState, useEffect } from 'react';
-import { fetchAllUserAPI } from '../../service/api.service';
 
+const UserTable = (props) => {
 
-const UserTable = () => {
-
-    // const [dataUsers, setDataUsers] = useState([
-    //     { _id: "ghost", fullName: 25, email: "hn" },
-    //     { _id: "vothien", fullName: 25, email: "hcm" }
-    // ]);
-    const [dataUsers, setDataUsers] = useState([]);
-
-    //empty array => run once
-    useEffect(() => {
-        console.log(">>> run useEffect 111")
-        loadUser();
-    }, []);
+    const { dataUsers } = props;
 
     const columns = [
         {
@@ -33,11 +20,6 @@ const UserTable = () => {
         }
     ];
 
-    const loadUser = async () => {
-        const res = await fetchAllUserAPI()
-        setDataUsers(res.data)
-    }
-    console.log(">>> run render 000")
     return (
         <Table
             columns={columns}
@@ -45,6 +27,7 @@ const UserTable = () => {
             rowKey={"_id"}
         />
     )
+
 }
 
 export default UserTable;
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,14 +1,31 @@
 import UserForm from "../component/user/user.form";
 import UserTable from "../component/user/user.table";
+import { fetchAllUserAPI } from '../service/api.service';
+import { useEffect, useState } from 'react';
+
 
 const UserPage = () => {
+    const [dataUsers, setDataUsers] = useState([]);
+
+    //empty array => run once
+    useEffect(() => {
+        loadUser();
+    }, []);
+
+    const loadUser = async () => {
+        const res = await fetchAllUserAPI()
+        setDataUsers(res.data)
+    }
+
+
+    // lift-up state 
     return (
         <div style={{ padding: "20px" }}>
-            <UserForm />
-            <UserTable />
+            <UserForm loadUser={loadUser} />
+            <UserTable dataUsers={dataUsers} />
         </div>
-
     )
+
 }
 
 export default UserPage;
