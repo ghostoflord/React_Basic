aad1e3e9801a0150d7708351da972ecb47875702
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-21 22:26:11 +0700

    80 update

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -22,6 +22,12 @@ const updateUserAPI = (_id, fullName, phone) => {
     return axios.put(URL_BACKEND, data);
 }
 
+const deleteUserAPI = (id) => {
+    const URL_BACKEND = `/api/v1/user/${id}`;//backtick
+    return axios.delete(URL_BACKEND);
+}
+
+
 const fetchAllUserAPI = () => {
     const URL_BACKEND = "/api/v1/user";
     return axios.get(URL_BACKEND);
@@ -29,5 +35,5 @@ const fetchAllUserAPI = () => {
 
 
 export {
-    createUserAPI, updateUserAPI, fetchAllUserAPI
+    createUserAPI, updateUserAPI, fetchAllUserAPI, deleteUserAPI
 }
