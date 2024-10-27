0a4f18230bd4ec66138559d0bf49414d0890b7ca
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-27 17:02:14 +0700

    92 Design Login  Basic

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,6 @@
-import { Button, Input, Form, notification, Row, Col } from "antd";
+import { Button, Input, Form, notification, Row, Col, Divider } from "antd";
 import { registerUserAPI } from "../service/api.service";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [form] = Form.useForm();
@@ -33,9 +33,10 @@ const RegisterPage = () => {
             form={form}
             layout="vertical"
             onFinish={onFinish}
-            style={{ margin: "10px" }}
+            style={{ margin: "30px" }}
         // onFinishFailed={onFinishFailed}
         >
+            <h3 style={{ textAlign: "center" }}>Đăng ký tài khoản</h3>
             <Row justify={"center"}>
                 <Col xs={24} md={8} >
                     <Form.Item
@@ -109,6 +110,9 @@ const RegisterPage = () => {
                             onClick={() => form.submit()}
                             type="primary">Register</Button>
                     </div>
+                    <Divider />
+                    <div>Đã có tài khoản? <Link to={"/login"}>Đăng nhập tại đây</Link></div>
+
                 </Col>
             </Row>
         </Form>
