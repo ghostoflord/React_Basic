df44d739a400f1724a807cf50c418af11327839c
ghostoflord <ghost2892003zzz@gmail.com>
2024-10-30 21:19:03 +0700

    view book

--- src/pages/book.jsx
+++ src/pages/book.jsx
@@ -1,6 +1,27 @@
+import BookTable from "../component/book/book.table";
+import { fetchAllBookAPI } from '../service/api.service';
+import { useEffect, useState } from 'react';
 const BookPage = () => {
+    const [dataBooks, setDataBooks] = useState([]);
+    const [current, setCurrent] = useState(1);
+    const [pageSize, setPageSize] = useState(5);
+    const [total, setTotal] = useState(0);
+    //empty array => run once
+    useEffect(() => {
+        loadBook();
+    }, [current, pageSize]); //[] + condition
+    const loadBook = async () => {
+        const res = await fetchAllBookAPI(current, pageSize);
+        if (res.data) {
+            setDataBooks(res.data.result);
+            setCurrent(res.data.meta.current);
+            setPageSize(res.data.meta.pageSize);
+            setTotal(res.data.meta.total);
+        }
+
+    }
     return (
-        <div>book page</div>
+        <BookTable loadBook={loadBook} />
     )
 }
 
