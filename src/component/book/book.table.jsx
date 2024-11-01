0fb9684b0443867e322499264b547f156391c44c
ghostoflord <ghost2892003zzz@gmail.com>
2024-11-01 16:03:50 +0700

    mupdate book uncontrol

--- src/component/book/book.table.jsx
+++ src/component/book/book.table.jsx
@@ -6,6 +6,7 @@ import BookDetail from "./book.detail";
 import CreateBookControl from "./create.book.control";
 import CreateBookUnControl from "./create.book.uncontrol";
 import UpdateBookControl from "./update.book.control";
+import UpdateBookUnControl from "./update.book.uncontrol";
 
 const BookTable = () => {
 
@@ -179,7 +180,14 @@ const BookTable = () => {
                 setIsCreateOpen={setIsCreateOpen}
                 loadBook={loadBook}
             />
-            <UpdateBookControl
+            {/* <UpdateBookControl
+                isModalUpdateOpen={isModalUpdateOpen}
+                setIsModalUpdateOpen={setIsModalUpdateOpen}
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+                loadBook={loadBook}
+            /> */}
+            <UpdateBookUnControl
                 isModalUpdateOpen={isModalUpdateOpen}
                 setIsModalUpdateOpen={setIsModalUpdateOpen}
                 dataUpdate={dataUpdate}
